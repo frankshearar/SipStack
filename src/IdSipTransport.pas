349660d96dc3747737d2eee26342ce5ec84b1125
FrankS <FrankS>
2003-12-24 13:46:33 +0000

    Simplified sending of UDP responses.

--- src/IdSipTransport.pas
+++ src/IdSipTransport.pas
@@ -860,32 +860,28 @@ end;
 
 procedure TIdSipUDPTransport.SendResponse(const R: TIdSipResponse);
 var
-  Client: TIdUdpClient;
+  Host: String;
+  Port: Cardinal;
 begin
   inherited SendResponse(R);
 
-  Client := TIdUdpClient.Create(nil);
-  try
-    if R.LastHop.HasReceived then begin
-      Client.Host := R.LastHop.Received;
+  if R.LastHop.HasReceived then begin
+    Host := R.LastHop.Received;
 
-      // cf RFC 3581 section 4.
-      // TODO: this isn't quite right. We have to send the response
-      // from the ip/port that the request was received on.
-      if R.LastHop.HasRport then
-        Client.Port := R.LastHop.RPort
-      else
-        Client.Port := R.LastHop.Port;
-    end
-    else begin
-      Client.Host := R.LastHop.SentBy;
-      Client.Port := R.LastHop.Port;
-    end;
-
-    Client.Send(R.AsString);
-  finally
-    Client.Free;
+    // cf RFC 3581 section 4.
+    // TODO: this isn't quite right. We have to send the response
+    // from the ip/port that the request was received on.
+    if R.LastHop.HasRport then
+      Port := R.LastHop.RPort
+    else
+      Port := R.LastHop.Port;
+  end
+  else begin
+    Host := R.LastHop.SentBy;
+    Port := R.LastHop.Port;
   end;
+
+  Self.Transport.Send(Host, Port, R.AsString);
 end;
 
 procedure TIdSipUDPTransport.SetPort(const Value: Cardinal);
