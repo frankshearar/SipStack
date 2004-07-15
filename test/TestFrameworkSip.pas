af38a9ba629060c9315d875292de3e4ebb0e8ba0
FrankS <FrankS>
2004-07-15 16:01:44 +0000

    *** empty log message ***

--- test/TestFrameworkSip.pas
+++ test/TestFrameworkSip.pas
@@ -64,7 +64,7 @@ type
   end;
 
   TIdSipTestOptionsListener = class(TIdInterfacedObject,
-                                         IIdSipOptionsListener)
+                                    IIdSipOptionsListener)
   private
   private
     fAuthenticationChallenge: Boolean;
