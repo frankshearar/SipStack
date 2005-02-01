5f19f6f38cc7305c12dc2a6058b8aa03e9f160a2
FrankS <FrankS>
2005-02-01 15:58:51 +0000

    Ignore. Whitespace change.

--- src/IdSipDialog.pas
+++ src/IdSipDialog.pas
@@ -327,7 +327,6 @@ begin
   Result.CopyHeaders(Self.InitialRequest,
                      ProxyAuthorizationHeader);
 
-
   // We create an ACK as an in-dialog request. Further, an ACK to a 2xx
   // _cannot_ match a transaction since the 2xx _terminated_ the transaction.
   // Therefore we use a completely new branch/transaction ID.
