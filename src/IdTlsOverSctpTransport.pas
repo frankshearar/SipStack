babfc1dadab4d072ca7ebae814722438c2c983e9
FrankS <FrankS>
2005-03-10 16:59:04 +0000

    Arg. Wrongly named file. Will remove immediately after this.

---
+++ src/IdTlsOverSctpTransport.pas
@@ -0,0 +1,34 @@
+unit IdTlsOverSctpTransport;
+
+interface
+
+uses
+  IdSipMessage, IdSipTransport;
+
+type
+  // RFC 3436
+  // This class only exists, at the moment, to ensure that we don't assume that
+  // TLS over TCP is the only secure transport.
+  TIdSipTlsOverSctpTransport = class(TIdSipSctpTransport)
+  public
+    class function IsSecure: Boolean; override;
+
+    function GetTransportType: String; override;
+  end;
+
+implementation
+
+class function TIdSipTlsOverSctpTransport.IsSecure: Boolean;
+begin
+  Result := true;
+end;
+
+function TIdSipTlsOverSctpTransport.GetTransportType: String;
+begin
+  Result := TlsOverSctpTransport;
+end;
+
+initialization
+  TIdSipTransport.RegisterTransport(TlsOverSctpTransport,
+                                    TIdSipTlsOverSctpTransport);
+end.
