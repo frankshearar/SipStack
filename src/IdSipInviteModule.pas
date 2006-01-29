f33a1ceaec403afddad25fb11392be44025e1ae0
FrankS <FrankS>
2006-01-29 11:15:07 +0000

    Corrected copyright year in IdSipInviteModule.

--- src/IdSipInviteModule.pas
+++ src/IdSipInviteModule.pas
@@ -1,5 +1,5 @@
 {
-  (c) 2004 Directorate of New Technologies, Royal National Institute for Deaf people (RNID)
+  (c) 2005 Directorate of New Technologies, Royal National Institute for Deaf people (RNID)
 
   The RNID licence covers this unit. Read the licence at:
       http://www.ictrnid.org.uk/docs/gw/rnid_license.txt
