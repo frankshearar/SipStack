60b482a4336341cedecc017a5abf91919d436754
FrankS <FrankS>
2006-02-28 19:35:32 +0000

    UdpTransport no longer uses ServerNotifier.

--- src/IdSipUdpTransport.pas
+++ src/IdSipUdpTransport.pas
@@ -12,8 +12,8 @@ unit IdSipUdpTransport;
 interface
 
 uses
-  Classes, IdSipLocator, IdSipMessage, IdSipServerNotifier, IdSipTransport,
-  IdSocketHandle,IdTimerQueue, IdUDPServer, SysUtils;
+  Classes, IdSipLocator, IdSipMessage, IdSipTransport, IdSocketHandle,IdTimerQueue,
+  IdUDPServer, SysUtils;
 
 type
   TIdSipUdpServer = class;
