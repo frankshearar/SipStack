6bdea07aebb6f2a750e4a04a7f15ae2be8fa2751
FrankS <FrankS>
2007-01-29 12:02:23 +0000

    Can't have too many class comments!

--- src/IdSipUserAgent.pas
+++ src/IdSipUserAgent.pas
@@ -80,7 +80,10 @@ type
   //   HostName: talkinghead1.tessier-ashpool.co.luna
   //   HostName: 192.168.1.1
   //   Listen: <transport name><SP><host|IPv4 address|IPv6 reference|AUTO>:<port>
-  //   MappedRoute: <route>/<netmask|number of bits> <gateway> [<port>]
+  //   Listen: UDP AUTO 
+  //   Listen: UDP 127.0.0.1
+  //   Listen: TCP [::1]:5060
+  //   MappedRoute: <route>/<netmask|number of bits><SP><gateway>[<SP><port>]
   //   MappedRoute: 192.168.0.0/16 192.168.1.1 5060
   //   MappedRoute: 192.168.0.0/255.255.255.0 192.168.1.1
   //   MappedRoute: ::/0 2002:deca:fbad::1 15060  
