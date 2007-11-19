fafe81f3a7a2a4da05b3480ccae6cfa3e8b3e6f4
franks <franks>
2007-11-19 11:59:13 +0000

    Unused local variables.

--- src/IdSdp.pas
+++ src/IdSdp.pas
@@ -2811,8 +2811,6 @@ begin
 end;
 
 class function TIdSdpParser.IsByteString(const Token: String): Boolean;
-var
-  I: Integer;
 begin
 //   byte-string =         1*(0x01..0x09|0x0b|0x0c|0x0e..0xff)
 //                         ;any byte except NUL, CR or LF
@@ -2887,8 +2885,6 @@ begin
 end;
 
 class function TIdSdpParser.IsPhone(const Token: String): Boolean;
-var
-  I: Integer;
 begin
   Result := Length(Token) >= 3;
 
