68e9a5cad738ab0504b568cd5e1602eaf8adc37e
FrankS <FrankS>
2009-03-26 12:45:17 +0000

    Fix ListenTo for IPv6 addresses (which must write out _references_). I can't imagine why I didn't finish implementing this; the tests were in place!

--- src/SipConfigUtils.pas
+++ src/SipConfigUtils.pas
@@ -22,7 +22,26 @@ function NameServer(Address: String; Port: Cardinal): String;
 implementation
 
 uses
-  IdSipUserAgent, SysUtils;
+  IdSimpleParser, IdSipUserAgent, SysUtils;
+
+//******************************************************************************
+//* Unit procedures/functions                                                  *
+//******************************************************************************
+//* Unit private procedures/functions ******************************************
+
+function ProtectIPv6Address(S: String): String;
+begin
+  // If S is an IPv6 address (so, something like "::1"), return an IPv6
+  // _reference_.
+  // Otherwise, just return S.
+
+  if TIdIPAddressParser.IsIPv6Address(S) then
+    Result := Format('[%s]', [S])
+  else
+    Result := S;
+end;
+
+//* Unit public procedures/functions *******************************************
 
 function Directive(Name, Value: String): String;
 begin
@@ -31,12 +50,12 @@ end;
 
 function ListenOn(Transport, Address: String): String;
 begin
-  Result := Directive(ListenDirective, Format('%s %s', [Transport, Address]));
+  Result := Directive(ListenDirective, Format('%s %s', [Transport, ProtectIPv6Address(Address)]));
 end;
 
 function ListenOn(Transport, Address: String; Port: Cardinal): String;
 begin
-  Result := Directive(ListenDirective, Format('%s %s:%d', [Transport, Address, Port]));
+  Result := Directive(ListenDirective, Format('%s %s:%d', [Transport, ProtectIPv6Address(Address), Port]));
 end;
 
 function NameServer(Address: String; Port: Cardinal): String;
